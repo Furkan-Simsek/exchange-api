f13cc00b112ac36e182182ba5dbc48af34294bfa
Furkan Şimşek <tcfurkansimsek+github@gmail.com>
2022-10-11 20:33:48 +0300

    Update exchange.py

--- exchange.py
+++ exchange.py
@@ -8,8 +8,7 @@ class RealTimeCurrencyConverter():
         self.currencies = self.data['rates']
     def convert(self, from_currency, to_currency, amount):
         initial_amount = amount
-        # first convert it into USD if it is not in USD.
-        # because our base currency is USD
+
         if from_currency != 'USD':
             amount = amount / self.currencies[from_currency]
 
